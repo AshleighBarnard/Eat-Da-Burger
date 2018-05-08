b31638cfc814d5cc8a8b15affcb21e14fe759403
Ashleigh Barnard <ashleighnbarnard@gmail.com>
2018-05-08 12:51:40 -0400

    broken

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,77 +1,44 @@
-var db = require("../models");
 
-module.exports = function(app) {
-	app.get("/", function(req, res) {
-		db.burger.findAll({
-			include: [db.customer]
-		}).then(function(data) {
-			res.render("index", {burgers: data});
-		})
-	});
+// Node Dependencies
+var express = require('express');
+var router = express.Router();
+var burger = require('../models/burger.js');
 
-	app.post("/", function(req, res) {
-		db.customer.create({
-			name: "placeholder"
-		}).then(function(data) {
-			var customerid = data.dataValues.id;
-			db.burger.create({
-				burger_name: req.body.name,
-				customerId: customerid
-			}).then(function(data) {
-				res.redirect("/");
-			});
-		});
-	});
 
-	app.put("/:id", function(req, res) {
-		if (req.body.customer !== undefined) {
-			db.customer.update(
-			{
-				name: req.body.customer
-			},
-			{
-				where: {
-					id: req.params.id
-				}
-			}).then(function() {
-				db.burger.update(
-					{
-						devoured: req.body.boolean === "true"
-					},
-					{
-						where: {
-							id: req.params.id
-						}
-					}
-				).then(function(data) {
-					res.redirect("/");
-				});
-			});
-		}
+// Create routes
+// ----------------------------------------------------
+// Index Redirect
+router.get('/', function (req, res) {
+  res.redirect('/index');
+});
 
-		else {
-			db.burger.update(
-				{
-					devoured: req.body.boolean === "true"
-				},
-				{
-					where: {
-						id: req.params.id
-					}
-				}
-			).then(function(data) {
-				res.redirect("/");
-			});
-		}
-	});
 
-	app.delete("/:id", function(req, res) {
-		db.burger.destroy({
-			where: {
-				id: req.params.id
-			}
-		}).then(function(data) {
-			res.redirect("/");
-		});
-	});
-};
+// Index Page (render all burgers to DOM)
+router.get('/index', function (req, res) {
+  burger.selectAll(function(data) {
+    var hbsObject = { burgers: data };
+    //console.log(hbsObject);
+    res.render('index', hbsObject);
+  });
+});
+
+
+// Create a New Burger
+router.post('/burger/create', function (req, res) {
+  burger.insertOne(req.body.burger_name, function() {
+    res.redirect('/index');
+  });
+});
+
+
+// Devour a Burger
+router.post('/burger/eat/:id', function (req, res) {
+  burger.updateOne(req.params.id, function() {
+    res.redirect('/index');
+  });
+});
+// ----------------------------------------------------
+
+
+// Export routes
+module.exports = router;
